a60d15875da29f5e54b524bb95172d21175f14cf
sergey <coteica555@gmail.com>
2025-10-19 18:31:22 +0300

    3classwork

---
+++ classwork/3_/vite-project/vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from "vite";
+import { viteSingleFile } from "vite-plugin-singlefile";
+
+export default defineConfig({
+    plugins: [
+        viteSingleFile(),
+    ],
+    base: "./",
+    build: {
+        outDir: "dist",
+        sourcemap: false,
+        assetsInlineLimit: 4096,
+        emptyOutDir: true,
+    },
+});
